20f005381213b6ea30cad2028a270a2c408bd48b
Vunb <nhubaovu@gmail.com>
2018-08-03 20:53:04 +0700

    setup rive api server for testing

--- test/index.js
+++ test/index.js
@@ -1,7 +1,11 @@
-var should = require('chai').should(),
-    expect = require('chai').expect,
-    mongoose = require('mongoose'),
-    mockgoose = require('./mockgoose');
+const path = require('path');
+const should = require('chai').should();
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+const mockgoose = require('./mockgoose');
+
+const kites = require('kites');
+const request = require('supertest');
 
 /**
  * Define a model
@@ -10,15 +14,37 @@ const Cat = mongoose.model('Cat', {
     name: String
 });
 
-describe('User functions', function () {
+var app;
+
+describe('Rivebot CE API', function () {
     before(async () => {
-        console.log('Start')
         await mockgoose.setup();
+        await kites({
+                rootDirectory: path.resolve(__dirname, '../'),
+                configFile: path.resolve(__dirname, '../kites.config.json'),
+                loadConfig: true,
+                discover: true
+            })
+            .use((kites) => {
+                // barrier which prevents kites auto create connection
+                kites.on('datasource:connect', () => {
+                    kites.logger.info('Use mockgoose connection for development! Do nothing.');
+                });
+            })
+            .init()
+            .then(function (kites) {
+                app = kites.express.app
+
+                kites.logger.info('Rivebot Server is Ready!!!');
+                return kites;
+            })
     });
 
     after(async () => {
         console.log('Done')
         await mockgoose.teardown();
+        // exit kites
+        process.exit(0);
     });
 
     it("should create a cat foo", function (done) {
@@ -54,10 +80,36 @@ describe('User functions', function () {
         });
     });
 
-    // it("reset", function (done) {
-    //     mockgoose.helper.reset().then(function () {
-    //         done();
-    //     });
-    // });
+    it('ping api server', (done) => {
+        request(app)
+            .get('/api/ping')
+            .end(function (err, res) {
+                expect(res.statusCode).to.equal(200);
+                expect(res.body.msg).to.be.eq('pong');
+                done(err);
+            });
+    });
+
+    it('init conversation', (done) => {
+        request(app)
+            .get('/api/nlu/chat/init_conversation')
+            .query({
+                context: {},
+                currentNode: '',
+                extractedParameters: {},
+                fromBot: false,
+                input: 'init_conversation',
+                intent: {},
+                speechResponse: '',
+                to: 'coco@yeu.ai',
+            })
+            .end(function (err, res) {
+                console.log(err, res.body);
+                expect(res.statusCode).to.equal(200);
+                expect(res.body.input).to.be.eq('init_conversation');
+                expect(res.body.complete).to.be.eq(true);
+                done(err);
+            });
+    })
 
 });
